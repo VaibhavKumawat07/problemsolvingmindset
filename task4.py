c3822edb29183adb2d298b25d0bf74d0036a0baa
Vaibhav Kumawat <vaibhavkumawat474@gmail.com>
2025-02-06 13:50:31 +0530

    Task 4 completed

---
+++ task4.py
@@ -0,0 +1,5 @@
+# Ek string variable create karein jo user input se college education ke problems ko capture kare
+
+college_problem = input("Enter your college problem do you want to solve").lower()
+
+print(college_problem)
